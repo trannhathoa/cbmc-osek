adfdc0e83b41c0d2842a508f8495675328242e0f
trannhathoa <trannhathoa@gmail.com>
2025-08-18 08:38:26 +0700

    add motivating example

---
+++ Experiments/motivatingexample.cpp
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include "_system.h"
+
+int x = 0;
+TASK t1(){
+  assert(x==0);
+  x = 1;
+  ActivateTask(_t2);
+  assert(x==1);
+  x = 2;
+}
+
+TASK t2(){
+  assert(x==2);
+  x = 3;
+}
